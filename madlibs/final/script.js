edcc0ebc076960d20c0bb40530f539e03f6981f3
jay <jlight413@gmail.com>
2025-02-10 22:16:30 -0800

    madlibs js update

--- madlibs/final/script.js
+++ madlibs/final/script.js
@@ -7,20 +7,29 @@
 
     const formPg1 = document.querySelector('#formPg1');
     const formPg2 = document.querySelector('#formPg2');
+    const form = document.querySelector('form');
 
-    formPg1.addEventListener('submit', function(event){
+    const navs = document.querySelectorAll('.nav');
+    const pg1next = navs[0];
+    const pg2prev = navs[1];
+    const pg2submit = navs[2];
+    const pg3tryAgain = navs[3];
+
+    pg1next.addEventListener('click', function(event){
+        event.preventDefault;
+        formPg1.classname = 'hidden';
+        formPg2.classname = 'shown';
+        console.log('i was clicked');
+    });
+
+    form.addEventListener('submit', function(event){
         event.preventDefault;
 
         
 
         pg1.classname = 'hidden';
-        pg2.classname = 'shown';
+        pg2.classname = 'hidden';
 
     });
-
-    // document.querySelector('#send-pg1').addEventListener('click', function(event){
-    //     event.preventDefault;
-
-    // });
 
 })();
