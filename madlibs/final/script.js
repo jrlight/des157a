637e1cb48e606c2f762061e86d5e860d240b0a47
jay <jlight413@gmail.com>
2025-02-12 00:35:08 -0800

    madlibs almost final. still needs mic graphic and ideally highlighting inputted words in final madlib

--- madlibs/final/script.js
+++ madlibs/final/script.js
@@ -13,7 +13,6 @@
     const navs = document.querySelectorAll('.nav');
     const pg1next = navs[0];
     const pg2prev = navs[1];
-    // const pg2submit = navs[2];
     const pg3tryAgain = navs[3];
 
     const allInputs = form.elements;    
@@ -35,8 +34,6 @@
 
     form.addEventListener('submit', function(event){
         event.preventDefault();
-        
-        console.log('i ran!');
 
         let emptyCounter = 0;
 
@@ -54,21 +51,23 @@
             
             //these ones are use multiple times so i'm naming them
             const fam = allValues[0].value;
-            const newJob = allValues[8].value;
+            const newJob = allValues[9].value;
             const action = allValues[13].value;
-            const goodAdj = allValues[14].value;
+            const badAdj = allValues[14].value;
             const oddName = allValues[15].value;
-            const badAdj = allValues[16].value;
+            const goodAdj = allValues[16].value;
 
             const set = document.querySelector('#set');
             const madLibPs = [document.createElement('p'), document.createElement('p'), document.createElement('p'), document.createElement('p'), document.createElement('p')];
+
+            // .insertAdjacentHTML(<hr>);
 
             const pTexts = [
                 document.createTextNode(`So my ${fam} is an ${allValues[1].value}. It’s pretty ${allValues[2].value}. The thing is, nowadays she doesn’t spend her time ${allValues[3].value} at ${allValues[4].value}, and instead spends her time ${allValues[5].value} at ${allValues[6].value} ${allValues[7].value} ${allValues[8].value}.`),
 
                 document.createTextNode(`…She’s now a ${newJob} of ${allValues[10].value} and ${allValues[11].value}.`),
 
-                document.createTextNode(` Now, that means I can look her up on RateMy${newJob}. It’s very ${allValues[12].value}, cuz then I can ${action} at all the ${goodAdj} reviews and go “THAT’S MY ${fam} YOU ${oddName}!” And then I ${action} at the ${badAdj} reviews and I’m like “aww, that’s my ${fam}, you ${oddName} ^-^”`),
+                document.createTextNode(` Now, that means I can look her up on RateMy${newJob}. It’s very ${allValues[12].value}, cuz then I can ${action} at all the ${badAdj} reviews and go “THAT’S MY ${fam} YOU ${oddName}!” And then I ${action} at the ${goodAdj} reviews and I’m like “aww, that’s my ${fam}, you ${oddName} ^-^”`),
 
                 document.createTextNode(`Now, she has a lot of ${goodAdj} reviews— not to brag, but her average is over ${allValues[17].value} stars.`),
 
@@ -88,10 +87,10 @@
                 element.value = '';
             }
 
-            postIt.children[1].className = 'shown';
+            postIt.children[1].className = 'hidden';
             postIt.children[2].className = 'hidden';
             postIt.children[3].className = 'hidden';
-
+            postIt.children[5].className = 'shown';
         }else if(emptyCounter === 1){
             postIt.children[1].className = 'hidden';
             postIt.children[2].className = 'hidden';
@@ -115,6 +114,19 @@
         inputs.className = 'shown';
         formPg1.className = 'shown';
         formPg2.className = 'hidden';
+
+        postIt.children[1].className = 'shown';
+        postIt.children[2].className = 'hidden';
+        postIt.children[3].className = 'hidden';
+        postIt.children[5].className = 'hidden';
+
+        
+        const set = document.querySelector('#set');
+        const pNumber = document.querySelectorAll('#set p').length;
+        
+        while(set.hasChildNodes()){
+            set.removeChild(set.firstChild);
+        }
     });
 
 })();
